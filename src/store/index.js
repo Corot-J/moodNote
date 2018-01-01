ffd643bd392da3b58ac8b6d528e097cb8ac8405f
Corot <843473864@qq.com>
2018-01-01 23:31:14 +0800

    图片路径修改

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,12 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state:{
         noteList:[
-            { id: 1, img: '../src/assets/head.jpg', date: '2017/12/11', mood: '今天心情不错'},
-            { id: 2, img: '../src/assets/tough.jpg', date: '2017/12/12', mood: '今天心情一般'},
-            { id: 3, img: '../src/assets/head.jpg', date: '2017/12/11', mood: '今天心情不错'},
-            { id: 4, img: '../src/assets/tough.jpg', date: '2017/12/12', mood: '今天心情一般'},
-            { id: 5, img: '../src/assets/head.jpg', date: '2017/12/11', mood: '今天心情不错'},
-            { id: 6, img: '../src/assets/tough.jpg', date: '2017/12/12', mood: '今天心情一般'}
+            { id: 1, img: 'static/img/head.jpg', date: '2017/12/11', mood: '今天心情不错'},
+            { id: 2, img: 'static/img/tough.jpg', date: '2017/12/12', mood: '今天心情一般'},
+            { id: 3, img: 'static/img/head.jpg', date: '2017/12/11', mood: '今天心情不错'},
+            { id: 4, img: 'static/img/tough.jpg', date: '2017/12/12', mood: '今天心情一般'},
+            { id: 5, img: 'static/img/head.jpg', date: '2017/12/11', mood: '今天心情不错'},
+            { id: 6, img: 'static/img/tough.jpg', date: '2017/12/12', mood: '今天心情一般'}
         ]
     },
     getters
